8100141021f2567651f53c3d2a8309c24e321162
rosevinnur <rosevinnur@rosevinnurs-iMac.local>
2020-10-22 00:58:46 +0300

    Some code for python test

---
+++ python_test.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+from newspaper import Article, Config
+
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+config = Config()
+config.browser_user_agent = user_agent
+config.request_timeout = 10
+
+news_url="https://news.google.com/rss/search?q=Russia+when:30d&hl=en-US&gl=US&ceid=US:en"
+Client=urlopen(news_url)
+xml_page=Client.read()
+Client.close()
+
+soup_page=soup(xml_page,"xml")
+news_list=soup_page.findAll("item")
+
+newArtics = ""
+for news in news_list:
+    newArtics +=  news.link.text + "\n"
+
+links = newArtics.splitlines( )
+
+text = ''
+for link in links:
+    a = Article(url=link, language='en', config=config)
+    try:
+        a.download()
+        a.parse()
+    except :
+        pass
+    text += a.text
+print(text)
